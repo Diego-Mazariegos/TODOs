4358f8ff81067ec5eb9fdf3715a221ed200d52da
Diego Mazariegos <dmazariegoslopez14@gmail.com>
2022-03-28 16:05:34 -0600

    Mi primer proyecto en react

---
+++ src/TodoItem/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './TodoItem.css';
+import { BsCheckLg, BsFillTrashFill } from "react-icons/bs"
+function TodoItem(props) {
+  return (
+    <li className="TodoItem">
+      <span
+        className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+        onClick={props.onComplete}
+      >
+        <BsCheckLg />
+      </span>
+      <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
+        {props.text}
+      </p>
+      <span
+        className="Icon Icon-delete"
+        onClick={props.onDelete}
+      >
+        <BsFillTrashFill />
+      </span>
+    </li>
+  );
+}
+
+export { TodoItem };
