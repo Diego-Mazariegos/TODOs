aa8069039d675ed880122435bbcf7cc77720778a
Diego Mazariegos <dmazariegoslopez14@gmail.com>
2022-03-28 16:29:07 -0600

    Arreglando el TODO search

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -13,7 +13,7 @@ function TodoSearch() {
   return (
     <input
       className="TodoSearch"
-      placeholder="Cebolla"
+      placeholder="Busca tu TODO"
       value={searchValue}
       onChange={onSearchValueChange}
     />
